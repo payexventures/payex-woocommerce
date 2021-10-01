44148a6b90f1a31a41142acf0470acae8b971f05
payexventures <72904856+payexventures@users.noreply.github.com>
2021-10-01 17:45:32 +0800

    add support for subscription payments

--- class-wc-payex-gateway.php
+++ class-wc-payex-gateway.php
@@ -7,12 +7,12 @@
  * @wordpress-plugin
  * Plugin Name:       Payex Payment Gateway for Woocommerce
  * Plugin URI:        https://payex.io
- * Description:       Accept Online Banking, Cards, EWallets and Instalments payments using Payex
- * Version:           1.1.9
+ * Description:       Accept Online Banking, Cards, EWallets, Instalments, and Subscription payments using Payex
+ * Version:           1.2.0
  * Requires at least: 4.7
  * Requires PHP:      7.0
- * Author:            Nedex Solutions
- * Author URI:        https://nedex.io
+ * Author:            Payex Ventures Sdn Bhd
+ * Author URI:        https://payex.io
  * License:           The MIT License (MIT)
  * License URI:       https://opensource.org/licenses/MIT
  */
@@ -56,6 +56,7 @@ function payex_init_gateway_class()
         const API_URL_SANDBOX = 'https://sandbox-payexapi.azurewebsites.net/';
         const API_GET_TOKEN_PATH = 'api/Auth/Token';
         const API_PAYMENT_FORM = 'api/v1/PaymentIntents';
+        const API_MANDATE_FORM = 'api/v1/Mandates';
         const HOOK_NAME = 'payex_hook';
 
         /**
@@ -84,6 +85,14 @@ function payex_init_gateway_class()
             $this->description = $this->get_option('description');
             $this->enabled = $this->get_option('enabled');
             $this->testmode = 'yes' === $this->get_option('testmode');
+
+            $this->supports = array( 
+                'products', 
+                'subscriptions',
+                // 'subscription_cancellation', 
+                // 'subscription_suspension', 
+                // 'subscription_reactivation',
+            );
 
             // This action hook saves the settings.
             add_action('woocommerce_update_options_payment_gateways_' . $this->id, array(
@@ -181,8 +190,6 @@ function payex_init_gateway_class()
 
             // we need it to get any order details.
             $order = wc_get_order($order_id);
-            $order_data = $order->get_data();
-            $order_items = $order->get_items();
             $url = self::API_URL;
 
             if ($this->get_option('testmode') === 'yes')
@@ -195,8 +202,14 @@ function payex_init_gateway_class()
             if ($token)
             {
                 // generate payex payment link.
-                $payment_link = $this->get_payex_payment_link($url, $order_data, $order_items, $this->get_return_url($order) , $order->get_checkout_payment_url() , WC()
-                    ->api_request_url(get_class($this)) , $token);
+                if (class_exists( 'WC_Subscriptions_Order' ) && WC_Subscriptions_Order::order_contains_subscription($order_id))
+                {
+                    $payment_link = $this->get_payex_mandate_link($url, $order, WC()->api_request_url(get_class($this)), $token);
+                }
+                else
+                {
+                    $payment_link = $this->get_payex_payment_link($url, $order, WC()->api_request_url(get_class($this)), $token);
+                }
 
                 // Redirect to checkout page on Payex.
                 return array(
@@ -231,6 +244,7 @@ function payex_init_gateway_class()
                     { // only mark order as completed if the order was not paid before.
                         $order->payment_complete();
                         $order->reduce_order_stock();
+                        WC_Subscriptions_Manager::activate_subscriptions_for_order( $order );
                     }
                 }
             }
@@ -240,16 +254,18 @@ function payex_init_gateway_class()
          * Generate Payment form link to allow users to Pay
          *
          * @param  string      $url             Payex API URL.
-         * @param  string      $order_data      Customer order data.
-         * @param  string      $order_items     Customer order items.
-         * @param  string      $accept_url      Return URL when customer completed payment.
-         * @param  string      $reject_url      Return URL when customer failed/cancelled payment.
+         * @param  string      $order           Customer order.
          * @param  string      $callback_url    Callback URL when customer completed payment.
          * @param  string|null $token           Payex token.
          * @return string
          */
-        private function get_payex_payment_link($url, $order_data, $order_items, $accept_url, $reject_url, $callback_url, $token = null)
+        private function get_payex_payment_link($url, $order, $callback_url, $token = null)
         {
+            $order_data = $order->get_data();
+            $order_items = $order->get_items();
+            $accept_url = $this->get_return_url($order);
+            $reject_url = $order->get_checkout_payment_url();
+
             if (!$token)
             {
                 $token = $this->getToken() ['token'];
@@ -268,7 +284,7 @@ function payex_init_gateway_class()
             {
                 $body = wp_json_encode(array(
                     array(
-                        "amount" => round($order_data['total'] * 100, 0) ,
+                        "amount" => round($order_data['total'] * 100, 0),
                         "currency" => $order_data['currency'],
                         "customer_id" => $order_data['customer_id'],
                         "description" => 'Payment for Order Reference:' . $order_data['order_key'],
@@ -297,6 +313,146 @@ function payex_init_gateway_class()
                 ));
 
                 $request = wp_remote_post($url . self::API_PAYMENT_FORM, array(
+                    'method' => 'POST',
+                    'timeout' => 45,
+                    'headers' => array(
+                        'Content-Type' => 'application/json',
+                        'Authorization' => 'Bearer ' . $token,
+                    ) ,
+                    'cookies' => array() ,
+                    'body' => $body
+                ));
+
+                if (is_wp_error($request) || 200 !== wp_remote_retrieve_response_code($request))
+                {
+                    error_log(print_r($request, true));
+                }
+                else
+                {
+                    $response = wp_remote_retrieve_body($request);
+                    $response = json_decode($response, true);
+                    if ($response['status'] == '99' || count($response['result']) == 0) error_log(print_r($request, true));
+                    return $response['result'][0]['url'];
+                }
+            }
+
+            return false;
+        }
+
+        /**
+         * Generate Mandate form link to allow users to Pay
+         *
+         * @param  string      $url             Payex API URL.
+         * @param  string      $order           Customer order.
+         * @param  string      $callback_url    Callback URL when customer completed payment.
+         * @param  string|null $token           Payex token.
+         * @return string
+         */
+        private function get_payex_mandate_link($url, $order, $callback_url, $token = null)
+        {
+            $order_data = $order->get_data();
+            $order_items = $order->get_items();
+            $accept_url = $this->get_return_url($order);
+            $reject_url = $order->get_checkout_payment_url();
+
+            if (!$token)
+            {
+                $token = $this->getToken() ['token'];
+            }
+
+            $items = array();
+
+            foreach ($order_items as $item_id => $item)
+            {
+                // order item data as an array
+                $item_data = $item->get_data();
+                array_push($items, $item_data);
+
+                $product_id = $item_data['product_id'];
+                if (WC_Subscriptions_Product::is_subscription($product_id))
+                {
+                    $subscription_period = get_post_meta( $product_id, '_subscription_period', true );
+                    $subscription_length = get_post_meta( $product_id, '_subscription_length', true );
+                    $subscription_trial_period = get_post_meta( $product_id, '_subscription_trial_period', true );
+                    $subscription_trial_length = get_post_meta( $product_id, '_subscription_trial_length', true );
+                }
+            }
+            
+            $initial_payment = WC_Subscriptions_Order::get_total_initial_payment( $order );
+            $price_per_period = WC_Subscriptions_Order::get_recurring_total( $order );
+            $subscription_interval = WC_Subscriptions_Order::get_subscription_interval( $order );
+            
+            if ($subscription_trial_length > 0)
+            {
+                $effective_date = date("Ymd", strtotime(date("Ymd")."+$subscription_trial_length $subscription_trial_period"));
+            }
+            else
+            {
+                $effective_date = date("Ymd");
+            }
+
+            switch ($subscription_period) 
+            {
+                case 'day':
+                    $frequency = 'DL';
+                    break;
+                case 'week':
+                    $frequency = 'WK';
+                    break;
+                case 'month':
+                    $frequency = 'MT';
+                    break;
+                case 'year':
+                    $frequency = 'YR';
+                    break;
+            }
+
+            $subscription_length -= 1;
+            $expiry_date = date("Ymd", strtotime($effective_date."+$subscription_length $subscription_period"));
+            if ($subscription_period == 'month' || $subscription_period == 'year') $expiry_date = date('Ymd', strtotime("-1 day", strtotime(date('Ym01', strtotime($effective_date)))));
+            if ($subscription_length <= 0) $expiry_date = null;
+            if ($initial_payment > 0) $debit_type = "AD";
+
+            if ($token)
+            {
+                $body = wp_json_encode(array(
+                    array(
+                        "max_amount" => round($price_per_period * 100, 0),
+                        "initial_amount" => round($initial_payment * 100, 0),
+                        "currency" => $order_data['currency'],
+                        "customer_id" => $order_data['customer_id'],
+                        "purpose" => 'Payment for Order Reference:' . $order_data['order_key'],
+                        "merchant_reference_number" => $order_data['id'],
+                        "frequency" => $frequency,
+                        "frequency_interval" => $subscription_interval,
+                        "effective_date" => $effective_date,
+                        "expiry_date" => $expiry_date,
+                        "max_frequency" => 1,
+                        "debit_type" => $debit_type,
+                        "customer_name" => $order_data['billing']['first_name'] . ' ' . $order_data['billing']['last_name'],
+                        "contact_number" => $order_data['billing']['phone'],
+                        "email" => $order_data['billing']['email'],
+                        "address" => $order_data['billing']['company'] . ' ' . $order_data['billing']['address_1'] . ',' . $order_data['billing']['address_2'],
+                        "postcode" => $order_data['billing']['postcode'],
+                        "city" => $order_data['billing']['city'],
+                        "state" => $order_data['billing']['state'],
+                        "country" => $order_data['billing']['country'],
+                        "shipping_name" => $order_data['shipping']['first_name'] . ' ' . $order_data['shipping']['last_name'],
+                        "shipping_address" => $order_data['shipping']['company'] . ' ' . $order_data['shipping']['address_1'] . ',' . $order_data['shipping']['address_2'],
+                        "shipping_postcode" => $order_data['shipping']['postcode'],
+                        "shipping_city" => $order_data['shipping']['city'],
+                        "shipping_state" => $order_data['shipping']['state'],
+                        "shipping_country" => $order_data['shipping']['country'],
+                        "return_url" => $accept_url,
+                        "accept_url" => $accept_url,
+                        "reject_url" => $reject_url,
+                        "callback_url" => $callback_url,
+                        "items" => $items,
+                        "source" => "wordpress"
+                    )
+                ));
+
+                $request = wp_remote_post($url . self::API_MANDATE_FORM, array(
                     'method' => 'POST',
                     'timeout' => 45,
                     'headers' => array(
